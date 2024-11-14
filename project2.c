4ae69d9650b6f653db70d333a034fefe98aff4ff
Connor <paarlberg.connor@gmail.com>
2024-11-13 20:31:56 -0500

    semaphores and structs implemented

--- project2.c
+++ project2.c
@@ -1,67 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/sem.h>
 
-// shared memory of shared resources
-typedef struct{
-    int mixer;
-    int pantry;
-    int refrigerator;
-    int bowl;
-    int spoon;
-    int oven;
-} resources;
-
-// shared memory of pantry ingredients
-// (assume there is enough for all bakers)
-typedef struct{
-    char flour;
-    char sugar;
-    char yeast;
-    char baking_soda;
-    char salt;
-    char cinnamon;
-} pantry;
-
-// shared memory of refrigerator ingredients 
-// (assume there is enough for all bakers)
-typedef struct{
-    char eggs;
-    char milk;
-    char butter;
-} refrigerator;
-
-
-void resource_values(){
-    resources shared_kitchen;
-
-    shared_kitchen.mixer = 2;
-    shared_kitchen.pantry = 1;
-    shared_kitchen.refrigerator = 2;
-    shared_kitchen.bowl = 3;
-    shared_kitchen.spoon = 5;
-    shared_kitchen.oven = 1;
-}
+#define PANTRY_INGREDIENTS 6
+#define REFRIGERATOR_INGREDIENTS 3
 
 void* baker_actions();
 
+int mixer_id, pantry_id, refrigerator_id, bowl_id, spoon_id, oven_id;
+
+struct sembuf lock = {0, -1, 0};  // P() operation
+struct sembuf unlock = {0, 1, 0}; // V() operation
+
+void create_semaphores(){
+    // create semaphores for shared kitchen reasources
+    mixer_id = semget(IPC_PRIVATE, 1, IPC_CREAT | 0666);
+    bowl_id = semget(IPC_PRIVATE, 1, IPC_CREAT | 0666);
+    spoon_id = semget(IPC_PRIVATE, 1, IPC_CREAT | 0666);
+    oven_id = semget(IPC_PRIVATE, 1, IPC_CREAT | 0666);
+    pantry_id = semget(IPC_PRIVATE, PANTRY_INGREDIENTS, IPC_CREAT | 0666);
+    refrigerator_id = semget(IPC_PRIVATE, REFRIGERATOR_INGREDIENTS, IPC_CREAT | 0666);
+
+    semctl(mixer_id, 0, SETVAL, 2); // Mixer = 2
+    semctl(bowl_id, 0, SETVAL, 3);  // Bowl = 3
+    semctl(spoon_id, 0, SETVAL, 5); // Spoon = 5
+    semctl(oven_id, 0, SETVAL, 1);  // Oven = 1
+
+    // create semaphores for ingredients in pantry
+    for (int i = 0; i < PANTRY_INGREDIENTS; i++) {
+        printf("pantry ingredient %d\n", i+1);
+        semctl(pantry_id, i, SETVAL, 1);
+    }
+
+    // create semaphores for ingredients in refrigerator
+    for (int i = 0; i < REFRIGERATOR_INGREDIENTS; i++) {
+        printf("refrigerator ingredient %d\n", i+1);
+        semctl(refrigerator_id, i, SETVAL, 1);
+    }
+}
+
+// represent two refrigerators with ingredients
+typedef struct{
+    const char* ingredients[REFRIGERATOR_INGREDIENTS];
+} refridgerator_struct;
+
+refridgerator_struct fridge[2] = {{"Eggs", "Milk", "Butter"}, {"Eggs", "Milk", "Butter"}};
+
+// represent a pantry with ingredients
+typedef struct{
+    const char* ingredients[PANTRY_INGREDIENTS];
+} pantry_struct;
+
+pantry_struct pantry = {{"Flour", "Sugar", "Yeast", "Baking Soda", "Salt", "Cinnamon"}};
+
+// represents a recipe made up of ingredients from frige and pantry
+typedef struct{
+    const char* name;
+    const char* pantry_ingredients[PANTRY_INGREDIENTS];
+    const char* refridgerator_ingredients[REFRIGERATOR_INGREDIENTS];
+} recipe;
+
+recipe list[] = {
+    {"cookies", {"Flour", "Sugar"}, {"Milk", "Butter"}},
+    {"pancakes", {"Flour", "Sugar", "Baking Soda", "Salt"}, {"Eggs", "Milk", "Butter"}},
+    {"homemade_pizza_dough", {"Yeast", "Sugar", "Salt"}, {}},
+    {"soft_pretzels", {"Flour", "Sugar", "Salt", "Yeast", "Baking Sdoa"}, {"Eggs"}},
+    {"cinnamon_rolls", {"Flour", "Sugar", "Salt", "Cinnamon"}, {"Butter", "Eggs"}}
+};
+
 void num_bakers(){
     char input[100];
-    pthread_t baker;
+    pthread_t *baker;
 
     printf("Enter the number if bakers: ");
     fgets(input, sizeof(input), stdin);
     int input_number = atoi(input);
 
-    int thread_count = 0;
-    while(thread_count != input_number){
-        if (pthread_create(&baker, NULL, baker_actions, NULL) != 0) {
-            perror("Faild to create baker");
+    baker = (pthread_t *) malloc(input_number * sizeof(pthread_t));
+
+    for(int i = 0; i < input_number; i++){
+        if (pthread_create(&baker[i], NULL, baker_actions, NULL) != 0) {
+            perror("Faild to create baker\n");
+            free(baker);
             exit(1);
         }
-        thread_count ++;
-        printf("Baker %d created\n", thread_count);
+        printf("Baker %d created\n", i+1);
     }
+    for (int i = 0; i < input_number; i++){
+        if(pthread_join(baker[i], NULL) != 0){
+            perror("Failed to join baker\n");
+            free(baker);
+            exit(1);
+        }
+    }
+    free(baker);
 }
 
 // TODO: fill out
@@ -74,13 +109,20 @@ void ramsied(){
 
 }
 
+void cleanup_semaphores(){
+    semctl(mixer_id, 0, IPC_RMID);
+    semctl(bowl_id, 0, IPC_RMID);
+    semctl(spoon_id, 0, IPC_RMID);
+    semctl(oven_id, 0, IPC_RMID);
+    semctl(pantry_id, 0, IPC_RMID);
+    semctl(refrigerator_id, 0, IPC_RMID);
+}
 
 int main() {
-    resource_values();
-
+    create_semaphores();
     num_bakers();
-
     ramsied();
 
+    cleanup_semaphores();
     return 0;
 }
