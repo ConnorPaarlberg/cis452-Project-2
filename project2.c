05a6f775e1d34f9404156254b21c727602e031b8
Connor <paarlberg.connor@gmail.com>
2024-11-21 20:33:09 -0500

    Fixed Bugs for baker actions

--- project2.c
+++ project2.c
@@ -16,7 +16,7 @@ void gather_refrigerator_ingredients(int, int);
 void mix_ingredients(int, int);
 void cook_recipe(int, int);
 
-int mixer_id, pantry_id, refrigerator_id, bowl_id, spoon_id, oven_id;
+int mixer_id, pantry_id, refrigerator_id_1, refrigerator_id_2, bowl_id, spoon_id, oven_id;
 
 void semLock(int semId){
     struct sembuf lock = {0, -1, 0};
@@ -41,17 +41,19 @@ void create_semaphores(){
     spoon_id = semget(IPC_PRIVATE, 1, IPC_CREAT | 0666);
     oven_id = semget(IPC_PRIVATE, 1, IPC_CREAT | 0666);
     pantry_id = semget(IPC_PRIVATE, 1, IPC_CREAT | 0666);
-    refrigerator_id = semget(IPC_PRIVATE, 1, IPC_CREAT | 0666);
+    refrigerator_id_1 = semget(IPC_PRIVATE, 1, IPC_CREAT | 0666);
+    refrigerator_id_2 = semget(IPC_PRIVATE, 1, IPC_CREAT | 0666);
 
-    semctl(mixer_id, 0, SETVAL, 2); // Mixer = 2
-    semctl(pantry_id, 0, SETVAL, 1); // Pantry = 1
-    semctl(refrigerator_id, 0, SETVAL, 2); // Refrigerator = 2
-    semctl(bowl_id, 0, SETVAL, 3);  // Bowl = 3
-    semctl(spoon_id, 0, SETVAL, 5); // Spoon = 5
-    semctl(oven_id, 0, SETVAL, 1);  // Oven = 1
+    semctl(mixer_id, 0, SETVAL, 2);
+    semctl(pantry_id, 0, SETVAL, 1);
+    semctl(refrigerator_id_1, 0, SETVAL, 1);
+    semctl(refrigerator_id_2, 0, SETVAL, 1);
+    semctl(bowl_id, 0, SETVAL, 3);
+    semctl(spoon_id, 0, SETVAL, 5);
+    semctl(oven_id, 0, SETVAL, 1);
 }
 
-// represents a recipe made up of ingredients from frige and pantry
+// represents a recipe made up of ingredients from refrigerator and pantry
 typedef struct{
     const char* name;
     const char* pantry_ingredients[PANTRY_INGREDIENTS];
@@ -60,9 +62,9 @@ typedef struct{
 
 recipe list[] = {
     {"cookies", {"Flour", "Sugar"}, {"Milk", "Butter"}},
-    {"pancakes", {"Flour", "Sugar", "Baking Soda", "Salt"}, {"Eggs", "Milk", "Butter"}},
+    {"pancakes", {"Flour", "Sugar", "Baking Soda", "Salt"}, {"Egg", "Milk", "Butter"}},
     {"homemade_pizza_dough", {"Yeast", "Sugar", "Salt"}, {}},
-    {"soft_pretzels", {"Flour", "Sugar", "Salt", "Yeast", "Baking Sdoa"}, {"Eggs"}},
+    {"soft_pretzels", {"Flour", "Sugar", "Salt", "Yeast", "Baking Soda"}, {"Egg"}},
     {"cinnamon_rolls", {"Flour", "Sugar", "Salt", "Cinnamon"}, {"Butter", "Eggs"}}
 };
 
@@ -71,7 +73,7 @@ void num_bakers(){
     pthread_t *baker;
     int *baker_ids;
 
-    printf("Enter the number if bakers: ");
+    printf("Enter the number of bakers: ");
     fgets(input, sizeof(input), stdin);
     int input_number = atoi(input);
 
@@ -100,7 +102,6 @@ void num_bakers(){
     free(baker_ids);
 }
 
-// TODO: fix null ingredient access (currently commented out in the ingredient retrival for loops)
 // TODO: unique color for each baker print statements
 void* baker_actions(void* baker){
     int num_recipes = sizeof(list) / sizeof(recipe);
@@ -108,8 +109,8 @@ void* baker_actions(void* baker){
 
     // each baker goes through all recipes
     for (int i = 0; i < num_recipes; i++){
-        gather_pantry_ingredients(i, baker_id);
         gather_refrigerator_ingredients(i, baker_id);
+        gather_pantry_ingredients(i, baker_id);
         mix_ingredients(i, baker_id);
         cook_recipe(i, baker_id);
 
@@ -121,54 +122,64 @@ void* baker_actions(void* baker){
     return NULL;
 }
 
-// TODO: fix so that refrigerator 1 and 2 are used. Currently, only refrigerator 1 is used
-void gather_pantry_ingredients(int recipe_index, int baker_id){
+void gather_refrigerator_ingredients(int recipe_index, int baker_id){
     printf("Baker %d is ready to gather ingredients to make %s\n", baker_id, list[recipe_index].name);
+    // only enter a refrigerator if the baker needs ingredients from a refrigerator
+    if (list[recipe_index].refridgerator_ingredients[0] != NULL){
+        // alternate between refrigerator 1 and 2
+        int chosen_refrigerator = baker_id % 2 + 1;
+        if (chosen_refrigerator == 1){
+            semLock(refrigerator_id_1);
+        } else{
+            semLock(refrigerator_id_2);
+        }
+        printf("Baker %d has entered refrigerator %d\n", baker_id, chosen_refrigerator);
+        // get refriger ingredients
+        for (int i = 0; i < REFRIGERATOR_INGREDIENTS; i++){
+            if (list[recipe_index].refridgerator_ingredients[i] != NULL){
+                printf("Baker %d took %s from refrigerator %d for the %s recipe\n", baker_id, 
+                list[recipe_index].refridgerator_ingredients[i], chosen_refrigerator, list[recipe_index].name);
+            }
+            // simulate baker walking to take next ingredient. If no more ingredients, 
+            // baker walks out of refrigerator 
+            sleep(1);
+        }
+        printf("Baker %d has gotten all refrigerator ingredients necessary for the %s recipe. Leaving refrigerator %d...\n", 
+        baker_id, list[recipe_index].name, chosen_refrigerator);
+        if (chosen_refrigerator == 1){
+            semUnlock(refrigerator_id_1);
+        } else{
+            semUnlock(refrigerator_id_2);
+        }
+    } else{
+        printf("Baker %d does not require any refrigerator ingredients for the %s recipe. Moving on...\n", baker_id, 
+        list[recipe_index].name);
+    }
+}
 
+
+void gather_pantry_ingredients(int recipe_index, int baker_id){
     // only enter the pantry if the baker needs ingredients from the pantry
     if (list[recipe_index].pantry_ingredients[0] != NULL){
         semLock(pantry_id);
         printf("Baker %d has entered the pantry\n", baker_id);
         // get pantry ingredients
         for (int i = 0; i < PANTRY_INGREDIENTS; i++){
-            const char* ingredient = list[recipe_index].pantry_ingredients[i];
-            // printf("Baker %d took %s from the pantry for the %s recipe\n", baker_id, ingredient, list[recipe_index].name);
-
+            if (list[recipe_index].pantry_ingredients[i] != NULL){
+                printf("Baker %d took %s from the pantry for the %s recipe\n", baker_id, list[recipe_index].pantry_ingredients[i], 
+                list[recipe_index].name);
+            }
             // simulate baker walking to take next ingredient. If no more ingredients, 
             // baker walks out of pantry 
             sleep(1);
         }
-        printf("Baker %d has gotten all pantry ingredients necessary for the %s recipe. Leaving pantry...\n", baker_id, list[recipe_index].name);
+        printf("Baker %d has gotten all pantry ingredients necessary for the %s recipe. Leaving pantry...\n", baker_id, 
+        list[recipe_index].name);
         semUnlock(pantry_id);
     } else{
-        printf("Baker %d does not require any pantry ingredients for the %s recipe. Moving on...\n", baker_id, list[recipe_index].name);
+        printf("Baker %d does not require any pantry ingredients for the %s recipe. Moving on...\n", baker_id, 
+        list[recipe_index].name);
     }
-}
-
-void gather_refrigerator_ingredients(int recipe_index, int baker_id){
-    // only enter a refrigerator if the baker needs ingredients from a refrigerator
-    if (list[recipe_index].refridgerator_ingredients[0] != NULL){
-        semLock(refrigerator_id);
-        int refrigerator_value = semctl(refrigerator_id, 0, GETVAL);
-
-        // Question: how would I get the value of the semaphore and put it here? so like it would say refrigerator 1
-        printf("Baker %d has entered refrigerator %d\n", baker_id, refrigerator_value);
-        // get refriger ingredients
-        for (int i = 0; i < REFRIGERATOR_INGREDIENTS; i++){
-            const char* ingredient = list[recipe_index].refridgerator_ingredients[i];
-            // printf("Baker %d took %s from refrigerator %d for the %s recipe\n", baker_id, ingredient, refrigerator_value, list[recipe_index].name);
-
-            // simulate baker walking to take next ingredient. If no more ingredients, 
-            // baker walks out of refrigerator 
-            sleep(1);
-        }
-        printf("Baker %d has gotten all refrigerator ingredients necessary for the %s recipe. Leaving refrigerator %d...\n", baker_id, list[recipe_index].name, refrigerator_value);
-        semUnlock(refrigerator_id);
-    } else{
-        printf("Baker %d does not require any refrigerator ingredients for the %s recipe. Moving on...\n", baker_id, list[recipe_index].name);
-    }
-
-    printf("Baker %d has gathered all ingredients for the %s recipe.\n", baker_id, list[recipe_index].name);
 }
 
 void mix_ingredients(int recipe_index, int baker_id){
@@ -208,7 +219,9 @@ void cleanup_semaphores(){
     semctl(spoon_id, 0, IPC_RMID);
     semctl(oven_id, 0, IPC_RMID);
     semctl(pantry_id, 0, IPC_RMID);
-    semctl(refrigerator_id, 0, IPC_RMID);
+    semctl(refrigerator_id_1, 0, IPC_RMID);
+    semctl(refrigerator_id_2, 0, IPC_RMID);
+
 }
 
 int main() {
@@ -216,6 +229,7 @@ int main() {
     num_bakers();
     ramsied();
 
+    printf("All Bakers have completed all recipes. Time to cleanup...\n");
     cleanup_semaphores();
     return 0;
 }
